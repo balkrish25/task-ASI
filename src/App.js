a2e884ca695bc85683d6edf1719ba02a385a3e0d
balkrish25 <balakrishnadongrekar@gmail.com>
2024-04-26 23:36:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./styles.css";
 function App() {
+  const [userData, setUserData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchUsers = async () => {
+    try {
+      const resp = await fetch(
+        "https://reqres.in/api/users?page=${currentPage}"
+      );
+      const data = await resp.json();
+      setUserData(data.data);
+      setTotalPages(data.total_pages);
+    } catch (error) {
+      console.log("Error fetching users", error);
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, [currentPage]);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = userData.filter((user) =>
+    `${user.first_name} ${user.last_name}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Info</h1>
+      <input
+        type="text"
+        placeholder="Search users..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <div className="user-list">
+        {filteredUsers.map((user) => (
+          <div className="user-card" key={user.id}>
+            <img src={user.avatar} alt="User Avatar" />
+            <div>
+              <h2>{`${user.first_name} ${user.last_name}`}</h2>
+              <p>Email: {user.email}</p>
+            </div>
+          </div>
+        ))}
+        <div>
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <span className="span-text">
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
